77e99c860773322456940d04b601b2b88ed4dfba
Raman Kumar <91066449+raman283313@users.noreply.github.com>
2022-09-29 20:04:06 +0530

    Add files via upload

---
+++ AreaOfRectangle.py
@@ -0,0 +1,36 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+a=input(Enter any value)
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+a=input("Enter any value")
+Enter any value45
+print (a)
+45
+
+========================================== RESTART: C:/Users/hp/AppData/Local/Programs/Python/Python310/rectangle area.py =========================================
+enter any value45
+45
+len=10
+bth=20
+area=2*(len+bth)
+print(area)
+60
+
+========================================= RESTART: C:/Users/hp/AppData/Local/Programs/Python/Python310/AverageOfNumber.py =========================================
+30
+
+========================================= RESTART: C:/Users/hp/AppData/Local/Programs/Python/Python310/AverageOfNumber.py =========================================
+29
+
+============================================ RESTART: C:/Users/hp/AppData/Local/Programs/Python/Python310/CelToFaran.py ===========================================
+Enter tempreture in degree1
+Traceback (most recent call last):
+  File "C:/Users/hp/AppData/Local/Programs/Python/Python310/CelToFaran.py", line 2, in <module>
+    tem=(tem*9//5)+32
+TypeError: unsupported operand type(s) for //: 'str' and 'int'
+
+============================================ RESTART: C:/Users/hp/AppData/Local/Programs/Python/Python310/CelToFaran.py ===========================================
+Enter tempreture in Celcius : 1
+33.0
+
